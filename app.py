2569ddf954e22626965da5256c7e5075ac613deb
luqui <luqui0008@gmail.com>
2025-09-13 14:45:08 -0300

    render

--- app.py
+++ app.py
@@ -296,4 +296,4 @@ def uploaded_file(filename):
     return send_from_directory(os.path.join(BASE_DIR, app.config['UPLOAD_FOLDER']), filename)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
